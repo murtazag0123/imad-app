e2d8a6c1744d5bf2857d9275b539e9c4acf022ea
murtazag0123 <38356733+murtazag0123@users.noreply.github.com>
2018-04-14 03:17:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,69 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne= {
+        title:'Article one| Murtaza',
+        heading:'Article one',
+        date:'April 14,2018',
+        content:   `
+            <p>
+                This is article one.<br>This is the content for my webapp.
+            </p>
+            <p>
+                lot of bullshit has been written in these meaningless paragraphs.
+                what else can i add to these paragraphs.theres alot to say but no time for that.wish i had more time for this.
+            </p>
+            <p>
+                Lets see where i can start to write about you.today is april 14th ,2018 time-2:09am.
+                alot has happened since a year.
+            </p>`
+        
+};
+
+function createtemplate (data) {
+
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmltemplate= `
+<html>
+    <head>
+        <title>
+               ${title}
+        </title>
+        <meta name="viewport" content="width=device-width,  initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href='/'>Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmltemplate
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
