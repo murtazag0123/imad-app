1203ed762bdb50a3d5f6926e1291bd8586e7f61c
murtazag0123 <38356733+murtazag0123@users.noreply.github.com>
2018-04-14 20:33:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,16 @@
-console.log('Loaded!');
+//counter code
+var button=document.getElementById('counter');
+var counter=0;
 
-//change the text of the main text div
-var element= document.getElementById('main-text');
-
-element.innerhtml='new value';
-
-//move the image
-var img=document.getElementById('madi');
-var marginleft= 0;
-function moveright(){
-    marginleft=marginleft+ 1;
-    img.style.marginleft=marginleft+'px';
-}
-img.onclick=function(){
-        var interval= setInterval(moveright, 50);
-        
+button.onclick=function(){
+    
+    //Make a request to the counter end point
+    
+    // capture the response and store it in a variable
+    
+    //render the variable in the correct span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
 };
